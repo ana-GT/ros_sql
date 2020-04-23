850520036a76039bf7d2e1e2090f313fcdfcb13d
Ana C. Huaman Quispe <ana@traclabs.com>
2020-04-23 15:15:56 -0500

    Fix one thing

--- src/ros_sql/type_map.py
+++ src/ros_sql/type_map.py
@@ -5,25 +5,25 @@ type_map = {
     'bool':
         s.Boolean(),
     'char':
-        s.SmallInteger(unsigned=True),
+        s.SmallInteger(), #(unsigned=True),
     'int8':
         s.SmallInteger(),
     'uint8':
-        s.SmallInteger(unsigned=True),
+        s.SmallInteger(),#(unsigned=True),
     'byte':
         s.SmallInteger(),
     'int16':
         s.Integer(),
     'uint16':
-        s.Integer(unsigned=True),
+        s.Integer(), #(unsigned=True),
     'int32':
         s.Integer(),
     'uint32':
-        s.Integer(unsigned=True),
+        s.Integer(), #(unsigned=True),
     'int64':
         s.BigInteger(),
     'uint64':
-        s.BigInteger(unsigned=True),
+        s.BigInteger(), #(unsigned=True),
     'float32':
         s.Float(precision=32),
     'float64':
